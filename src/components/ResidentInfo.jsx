b461aa03ab1dc42dff73f70cbbd8b402d5e38493
4RomanR <4rprogramming@gmail.com>
2023-12-09 12:56:59 -0400

    finish project

--- src/components/ResidentInfo.jsx
+++ src/components/ResidentInfo.jsx
@@ -22,11 +22,11 @@ const ResidentInfo = ({ resdURL }) => {
     <section className="relative border-2 border-green-500 text-white ">
       <img src={resdUser?.image} alt="" />
       {/* Status */}
-      <div className="flex items-center gap-2 relative bottom-16 w-[90px] left-1/2 -translate-x-1/2 bg-black/60 p-2 text-white  border-2 border-green-500">
+      <div className="flex justify-center items-center gap-2 relative bottom-16 w-[120px] left-1/2 -translate-x-1/2 bg-black/60 text-white  border-2 border-green-500">
         <div className={`h-3  aspect-square rounded-full ${status[resdUser?.status]}`}></div>
         <p>{resdUser?.status}</p>
       </div>
-      <h3 className="p-4">Resident Name</h3>
+      <h3 className="px-4 my-2">Resident Name</h3>
       <ul className="px-2 ">
         <li><span className="px-2">Species </span>{resdUser?.species}</li>
         <li><span className="px-2">Origin </span>{resdUser?.origin.name}</li>
--- src/components/Residents.jsx
+++ src/components/Residents.jsx
@@ -7,37 +7,51 @@ const Residents = ({ resd }) => {
 
   const { pages, resdCurrentPage } = paginationLogic(currentPage, resd)
 
-  const handlePage = (newPage)=>{
+  const handlePage = (newPage) => {
     setCurrentPage(newPage)
   }
   console.log(resd)
 
 
-  useEffect(()=>{
+  useEffect(() => {
     setCurrentPage(1)
   }, [resd])
 
 
   return (
-    <section className="max-w-[1000px] mx-auto ">
-      <section className="flex gap-4 flex-wrap place-content-center mx-auto">
-        {
-          resdCurrentPage.map((resdt) => <ResidentInfo key={resdt} resdURL={resdt} />)
-        }
+    <section>
+      <section className="max-w-[1000px] mx-auto ">
+        <img className="absolute -bottom-96 translate-x-96 opacity-30" src="./image/stars.png" alt="stars" />
+        <img className="absolute -bottom-60 translate-x-96 opacity-50" src="./image/gradient.png" alt="gradient" />
+        <section className="flex gap-4 flex-wrap place-content-center mx-auto">
+          {
+            resdCurrentPage.map((resdt) => <ResidentInfo key={resdt} resdURL={resdt} />)
+          }
 
+
+          <img className="absolute -left-56 opacity-50" src="./image/stars.png" alt="stars" />
+          <img className="absolute -left-60 opacity-50" src="./image/gradient.png" alt="gradient" />
+
+
+        </section>
+
+        {/* Pagination */}
+        <ul className="flex justify-center p-4 gap-6 flex-wrap items-center  ">
+          {
+            pages.map((page) => (
+              <li className="text-white" key={page}>
+                <button className={`${currentPage === page && "bg-green-950/70 py-2 px-4 text-white"}`} onClick={() => handlePage(page)}>{page}</button>
+              </li>
+
+            ))}
+        </ul>
 
       </section>
-      {/* Pagination */}
-      <ul className="flex justify-center p-4 gap-6 flex-wrap items-center  ">
-        {
-          pages.map((page) => (
-            <li className="text-white" key={page}>
-              <button className={`${currentPage === page && "bg-green-950/70 py-2 px-4 text-white" }`} onClick={() => handlePage(page)}>{page}</button>
-            </li>
+     
 
-          ))}
-      </ul>
+
     </section>
+
 
   )
 }
